e1aa43d639d8edb0426aac2018d85e22db8088de
Claudio Pupparo <claudio.pupparo@gmail.com>
2018-08-11 14:30:53 +0200

    West rotations completed

--- deponia-pidgeon-solver/tests/model/configuration_test.py
+++ deponia-pidgeon-solver/tests/model/configuration_test.py
@@ -29,7 +29,7 @@ class ConfigurationTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             conf.get_rotated_configuration(central_node)
 
-    def test_given_west_central_node_and_south_not_active_then_rotate(self):
+    def test__given_west_central_node_and_south_not_active__then_rotate(self):
         node_list = [node.Node(0, 2, is_center=False, is_active=False),  # top line
                      node.Node(1, 2, is_center=False, is_active=False),  # top line
                      node.Node(2, 2, is_center=False, is_active=False),  # top line
@@ -47,17 +47,16 @@ class ConfigurationTest(unittest.TestCase):
 
         conf = configuration.Configuration(node_list)
         rotated_conf = conf.get_rotated_configuration(central_node)
-        print(rotated_conf)
 
         self.assertEqual(True, (node.Node(0, 2, is_center=False, is_active=False) in rotated_conf.nodes))
         self.assertEqual(True, (node.Node(0, 1, is_center=True, is_active=True) in rotated_conf.nodes))
         self.assertEqual(True, (node.Node(1, 1, is_center=True, is_active=False) in rotated_conf.nodes))
         self.assertEqual(True, (node.Node(0, 0, is_center=False, is_active=True) in rotated_conf.nodes))
 
-    def test_given_west_central_node_and_south_active_and_east_not_active_then_rotate(self):
+    def test__given_west_central_node_and_south_active_and_east_not_active__then_rotate(self):
         node_list = [node.Node(0, 2, is_center=False, is_active=False),  # top line
                      node.Node(1, 2, is_center=False, is_active=False),  # top line
-                     node.Node(2, 2, is_center=False, is_active=False),  # top line
+                     node.Node(2, 2, is_center=False, is_active=True),  # top line
 
                      node.Node(0, 1, is_center=True, is_active=True),  # middle line
                      node.Node(1, 1, is_center=True, is_active=False),  # middle line
@@ -72,12 +71,61 @@ class ConfigurationTest(unittest.TestCase):
 
         conf = configuration.Configuration(node_list)
         rotated_conf = conf.get_rotated_configuration(central_node)
-        print(rotated_conf)
 
         self.assertEqual(True, (node.Node(0, 2, is_center=False, is_active=True) in rotated_conf.nodes))
         self.assertEqual(True, (node.Node(0, 1, is_center=True, is_active=True) in rotated_conf.nodes))
         self.assertEqual(True, (node.Node(1, 1, is_center=True, is_active=False) in rotated_conf.nodes))
         self.assertEqual(True, (node.Node(0, 0, is_center=False, is_active=False) in rotated_conf.nodes))
+
+    def test__given_west_central_node_and_south_east_active__then_rotate(self):
+        node_list = [node.Node(0, 2, is_center=False, is_active=False),  # top line
+                     node.Node(1, 2, is_center=False, is_active=False),  # top line
+                     node.Node(2, 2, is_center=False, is_active=False),  # top line
+
+                     node.Node(0, 1, is_center=True, is_active=True),  # middle line
+                     node.Node(1, 1, is_center=True, is_active=True),  # middle line
+                     node.Node(1, 2, is_center=True, is_active=True),  # middle line
+
+                     node.Node(0, 0, is_center=False, is_active=True),  # bottom line
+                     node.Node(1, 0, is_center=True, is_active=False),  # bottom line
+                     node.Node(2, 0, is_center=False, is_active=True),  # bottom line
+
+                     node.Node(1, -1, is_center=False, is_active=True)]  # ultra bottom line
+        central_node = node_list[3]  # rotate central node of left column
+
+        conf = configuration.Configuration(node_list)
+        rotated_conf = conf.get_rotated_configuration(central_node)
+        print(rotated_conf)
+
+        self.assertEqual(True, (node.Node(0, 2, is_center=False, is_active=True) in rotated_conf.nodes))
+        self.assertEqual(True, (node.Node(0, 1, is_center=True, is_active=True) in rotated_conf.nodes))
+        self.assertEqual(True, (node.Node(1, 1, is_center=True, is_active=True) in rotated_conf.nodes))
+        self.assertEqual(True, (node.Node(0, 0, is_center=False, is_active=False) in rotated_conf.nodes))
+
+    def test__given_west_central_node_and_south_east_north_active__then_rotate(self):
+        node_list = [node.Node(0, 2, is_center=False, is_active=True),  # top line
+                     node.Node(1, 2, is_center=False, is_active=False),  # top line
+                     node.Node(2, 2, is_center=False, is_active=False),  # top line
+
+                     node.Node(0, 1, is_center=True, is_active=True),  # middle line
+                     node.Node(1, 1, is_center=True, is_active=True),  # middle line
+                     node.Node(1, 2, is_center=True, is_active=True),  # middle line
+
+                     node.Node(0, 0, is_center=False, is_active=True),  # bottom line
+                     node.Node(1, 0, is_center=True, is_active=False),  # bottom line
+                     node.Node(2, 0, is_center=False, is_active=True),  # bottom line
+
+                     node.Node(1, -1, is_center=False, is_active=False)]  # ultra bottom line
+        central_node = node_list[3]  # rotate central node of left column
+
+        conf = configuration.Configuration(node_list)
+        rotated_conf = conf.get_rotated_configuration(central_node)
+        print(rotated_conf)
+
+        self.assertEqual(True, (node.Node(0, 2, is_center=False, is_active=True) in rotated_conf.nodes))
+        self.assertEqual(True, (node.Node(0, 1, is_center=True, is_active=True) in rotated_conf.nodes))
+        self.assertEqual(True, (node.Node(1, 1, is_center=True, is_active=True) in rotated_conf.nodes))
+        self.assertEqual(True, (node.Node(0, 0, is_center=False, is_active=True) in rotated_conf.nodes))
 
     def get_nodes(self):
         return [node.Node(0, 2, is_center=False, is_active=False),  # top line
